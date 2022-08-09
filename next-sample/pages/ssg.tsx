2b7d989ee6d0e3ad90c8b9ec8ba0b6dadaca3014
daichi <solar.eeclipse@gmail.com>
2022-08-10 03:50:23 +0900

    最新化

--- next-sample/pages/ssg.tsx
+++ next-sample/pages/ssg.tsx
@@ -1,20 +1,19 @@
-import { GetStaticProps, NextPage } from "next";
-import Head from "next/head";
+import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
 
 type SSGProps = {
   message: string;
 };
 
-//NextPageはNext.jsのPages向けの型
-//NextPage<props>でpropsが入るPageであることを明示
-
+// NextPageはNext.jsのPages向けの型
+// NextPage<props>でpropsが入るPageであることを明示
 const SSG: NextPage<SSGProps> = (props) => {
   const { message } = props;
   return (
     <div>
       <Head>
         <title>Static Site Generation</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
         <p>このページは静的サイト生成によってビルド時に生成されたページです</p>
