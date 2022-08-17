48ad7dffeba650bbaf67ada8b8aa1852ae0d7419
daichi <solar.eeclipse@gmail.com>
2022-08-18 03:24:41 +0900

    サンプルアプリの初期登録

--- next-emotion/.storybook/preview.js
+++ next-emotion/.storybook/preview.js
@@ -1,3 +1,27 @@
+const customViewports = {
+  Pixel5: {
+    name: 'Pixel5',
+    styles: {
+      width: '393px',
+      height: '851px',
+    },
+  },
+  iPhone13: {
+    name: 'iPhone13',
+    styles: {
+      width: '390px',
+      height: '844px',
+    },
+  },
+  iPadPro12_2021: {
+    name: 'iPad Pro 12 (2021)',
+    styles: {
+      width: '1024px',
+      height: '1366px',
+    },
+  },
+}
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +30,18 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: { viewports: customViewports },
+  backgrounds: {
+    default: 'white',
+    values: [
+    { name: 'white', value: '#ffffff'},
+    { name: 'black', value: '#333333' },
+    { name: 'gray', value: '#F3F5FB' },
+    { name: 'facebook', value: '#3b5998' },
+    { name: 'twitter', value: '#00aced' },
+  ]},
+  // darkMode: {
+  //   dark: { ...themes.dark, appBg: 'black' },
+  //   light: { ...themes.normal, appBg: 'white' }
+  // }
 }
